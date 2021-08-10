ffbc5dcfd57b5eba6fa9839473279d528c072886
Mansur Irzakhanov <hwimahw@gmail.com>
2021-08-10 16:16:58 +0300

    check

---
+++ src/main/java/Solution.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
